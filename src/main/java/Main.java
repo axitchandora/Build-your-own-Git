cdfc3902c55242effb0078e942c848bbbac567b3
Akshit Kumar Chandora <axitchandora@gmail.com>
2025-02-03 22:49:40 +0530

    Implement Git blob object retrieval with 'cat-file' command
    - Extracts object hash and determines storage path in `.git/objects/`.
    - Reads and decompresses blob objects using Zlib.
    - Parses and prints the content after removing the header.
    - Handles potential `IOException` during file operations.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,28 +1,42 @@
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.nio.file.Files;
+import java.util.zip.InflaterInputStream;
 
 public class Main {
-  public static void main(String[] args){
+    public static void main(String[] args) throws FileNotFoundException {
 
-     final String command = args[0];
+        final String command = args[0];
 
-     switch (command) {
-       case "init" -> {
-         final File root = new File(".git");
-         new File(root, "objects").mkdirs();
-         new File(root, "refs").mkdirs();
-         final File head = new File(root, "HEAD");
+        switch (command) {
+            case "init" -> {
+                final File root = new File(".git");
+                new File(root, "objects").mkdirs();
+                new File(root, "refs").mkdirs();
+                final File head = new File(root, "HEAD");
 
-         try {
-           head.createNewFile();
-           Files.write(head.toPath(), "ref: refs/heads/main\n".getBytes());
-           System.out.println("Initialized git directory");
-         } catch (IOException e) {
-           throw new RuntimeException(e);
-         }
-       }
-       default -> System.out.println("Unknown command: " + command);
-     }
-  }
+                try {
+                    head.createNewFile();
+                    Files.write(head.toPath(), "ref: refs/heads/main\n".getBytes());
+                    System.out.println("Initialized git directory");
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            case "cat-file" -> {
+                String hash = args[2];
+                String dirHash = hash.substring(0, 2);
+                String fileHash = hash.substring(2);
+                File blobObjectFile = new File(".git/objects/" + dirHash + "/" + fileHash);
+
+                try {
+                    String blob = new BufferedReader(new InputStreamReader(new InflaterInputStream(new FileInputStream(blobObjectFile)))).readLine();
+                    String content = blob.substring(blob.indexOf("\0") + 1);
+                    System.out.print(content);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            default -> System.out.println("Unknown command: " + command);
+        }
+    }
 }
